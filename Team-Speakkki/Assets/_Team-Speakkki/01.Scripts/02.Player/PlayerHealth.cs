63c80b5de307c7f7ce067f9be35c2e4cddfc3df1
Athame0802 <266697209+Athame0802@users.noreply.github.com>
2026-09-11 19:05:43 +0900

    feat: 파워업을 제외한 플레이어 구현

--- Team-Speakkki/Assets/_Team-Speakkki/01.Scripts/02.Player/PlayerHealth.cs
+++ Team-Speakkki/Assets/_Team-Speakkki/01.Scripts/02.Player/PlayerHealth.cs
@@ -5,25 +5,37 @@ namespace TeamSpeakkki.Chaewon
 {
     public class PlayerHealth : MonoBehaviour
     {
+        [SerializeField] private int maxHealth = 3;
         [SerializeField] private int health = 3;
-    
+
         public event Action<int> OnHealthChanged;
+        public event Action OnDied;
 
         public int Health => health;
     
         public void TakeDamage(int amount)
         {
-    
+            health -= amount;
+            OnHealthChanged?.Invoke(health);
+
+            if (health <= 0)
+            {
+                Die();
+            }
         }
     
         public void Heal(int amount)
         {
-    
+            bool isMaxOver = health + amount > maxHealth;
+            health = isMaxOver ? maxHealth : health + amount;
+
+            OnHealthChanged?.Invoke(health);
         }
     
         private void Die()
         {
-    
+            OnDied?.Invoke();
+            Destroy(gameObject);
         }
     }
 }
--- Team-Speakkki/Assets/_Team-Speakkki/01.Scripts/02.Player/PlayerStatus.cs
+++ Team-Speakkki/Assets/_Team-Speakkki/01.Scripts/02.Player/PlayerStatus.cs
@@ -6,6 +6,7 @@ namespace TeamSpeakkki.Chaewon
     public class PlayerStatus : MonoBehaviour
     {
         [SerializeField] private int coinCount = 0;
+        [SerializeField] private int maxCoinCount = 99;
         private IPlayerPowerUp currentPowerUp;
     
         public event Action<int> OnCoinChanged;
@@ -14,7 +15,10 @@ namespace TeamSpeakkki.Chaewon
     
         public void AddCoin(int amount)
         {
-    
+            bool isMaxOver = coinCount + amount > maxCoinCount;
+            coinCount = isMaxOver ? maxCoinCount : coinCount + amount;
+
+            OnCoinChanged?.Invoke(coinCount);
         }
 
         public void ChangePowerUp(IPlayerPowerUp powerUp)
