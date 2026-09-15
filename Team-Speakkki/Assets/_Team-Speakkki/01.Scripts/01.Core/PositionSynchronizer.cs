63749da334256838e8b9b048c8d2b072aacced4b
Athame0802 <266697209+Athame0802@users.noreply.github.com>
2026-09-15 17:29:46 +0900

    feat: 게임 씬 구성

--- Team-Speakkki/Assets/_Team-Speakkki/01.Scripts/01.Core/PositionSynchronizer.cs
+++ Team-Speakkki/Assets/_Team-Speakkki/01.Scripts/01.Core/PositionSynchronizer.cs
@@ -1,24 +1,27 @@
 ﻿using School.PositionSync;
 using TeamSpeakkki.Chaewon;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class PositionSynchronizer : MonoBehaviour
 {
     [SerializeField] private GameObject player;
     [SerializeField] private GameObject otherPlayerPrefab;
 
-    private Server server = NetworkManager.Instance.Server;
+    private Server server;
 
     // 만약 게임 도중에 플레이어가 들어올 수 있다면 List로 변경
-    private GameObject[] otherPlayers;
+    private List<GameObject> otherPlayers;
 
     public void Start()
     {
+        server = NetworkManager.Instance.Server;
+
         Info[] players = server.GetPos();
 
-        otherPlayers = new GameObject[players.Length];
+        otherPlayers = new(players.Length);
 
-        for (int i = 0; i < otherPlayers.Length; i++)
+        for (int i = 0; i < otherPlayers.Count; i++)
         {
             Vector3 otherPlayerPosition = new Vector3(players[i].X, players[i].Y, 0f);
 
@@ -35,7 +38,7 @@ public class PositionSynchronizer : MonoBehaviour
         server.SetPos(new Info(playerPosition.x, playerPosition.y));
 
         Info[] otherPlayerInfos = server.GetPos();
-        for (int i = 0; i < otherPlayers.Length; i++)
+        for (int i = 0; i < otherPlayers.Count && i < otherPlayerInfos.Length; i++)
         {
             Vector3 otherPlayerPosition = new Vector3(otherPlayerInfos[i].X, otherPlayerInfos[i].Y, 0f);
 
