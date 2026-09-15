00a155a5310174934034029720c8ca2f3e320658
Athame0802 <266697209+Athame0802@users.noreply.github.com>
2026-09-15 17:48:50 +0900

    fix: 위치 동기화 수정

--- Team-Speakkki/Assets/_Team-Speakkki/01.Scripts/01.Core/PositionSynchronizer.cs
+++ Team-Speakkki/Assets/_Team-Speakkki/01.Scripts/01.Core/PositionSynchronizer.cs
@@ -23,12 +23,13 @@ public class PositionSynchronizer : MonoBehaviour
 
         for (int i = 0; i < otherPlayers.Count; i++)
         {
+            Debug.Log("추가");
             Vector3 otherPlayerPosition = new Vector3(players[i].X, players[i].Y, 0f);
 
-            otherPlayers[i] = Instantiate(
+            otherPlayers.Add(Instantiate(
                 otherPlayerPrefab, 
                 otherPlayerPosition, 
-                Quaternion.identity);
+                Quaternion.identity));
         }
     }
 
@@ -38,9 +39,20 @@ public class PositionSynchronizer : MonoBehaviour
         server.SetPos(new Info(playerPosition.x, playerPosition.y));
 
         Info[] otherPlayerInfos = server.GetPos();
-        for (int i = 0; i < otherPlayers.Count && i < otherPlayerInfos.Length; i++)
+        for (int i = 0; i < otherPlayerInfos.Length; i++)
         {
             Vector3 otherPlayerPosition = new Vector3(otherPlayerInfos[i].X, otherPlayerInfos[i].Y, 0f);
+
+            if (i >= otherPlayers.Count)
+            {
+                Debug.Log("추가");
+                otherPlayers.Add(Instantiate(
+                otherPlayerPrefab,
+                otherPlayerPosition,
+                Quaternion.identity));
+
+                return;
+            }
 
             otherPlayers[i].transform.position = otherPlayerPosition;
         }
