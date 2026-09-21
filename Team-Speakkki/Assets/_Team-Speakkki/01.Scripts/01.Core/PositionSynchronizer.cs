ddb25b0f2e4b007ffbcdbbab46bb4c699a88db40
Athame0802 <266697209+Athame0802@users.noreply.github.com>
2026-09-21 18:31:33 +0900

    refactor: 플레이어 동기화 기능 수정
    - List에서 ID 기반 플레이어 조회로 변경

--- Team-Speakkki/Assets/_Team-Speakkki/01.Scripts/01.Core/PositionSynchronizer.cs
+++ Team-Speakkki/Assets/_Team-Speakkki/01.Scripts/01.Core/PositionSynchronizer.cs
@@ -8,53 +8,79 @@ public class PositionSynchronizer : MonoBehaviour
     [SerializeField] private GameObject player;
     [SerializeField] private GameObject otherPlayerPrefab;
 
-    // 만약 게임 도중에 플레이어가 들어올 수 있다면 List로 변경
-    private List<GameObject> otherPlayers;
+    private Dictionary<int, GameObject> otherPlayersGameObject = new(10);
+    private HashSet<int> activedOtherPlayersId = new(10);
 
     public void Start()
     {
         Server server = NetworkManager.Instance.Server;
 
-        Info[] players = server.GetPos();
+        Info[] otherPlayers = server.GetPos();
         
-        otherPlayers = new(players.Length);
-
-        for (int i = 0; i < otherPlayers.Count; i++)
-        {
-            Debug.Log("추가");
-            Vector3 otherPlayerPosition = new Vector3(players[i].X, players[i].Y, 0f);
-
-            otherPlayers.Add(Instantiate(
-                otherPlayerPrefab, 
-                otherPlayerPosition, 
-                Quaternion.identity));
-        }
+        foreach (Info otherPlayer in otherPlayers)
+            AddOtherPlayer(otherPlayer);
     }
 
     public void Update()
     {
         Server server = NetworkManager.Instance.Server;
 
-        Vector2 playerPosition = player.transform.position;
-        server.SetPos(new Info(playerPosition.x, playerPosition.y));
+        SyncThisClientPosition();
+        SyncOtherClientsPosition();
+    }
 
-        Info[] otherPlayerInfos = server.GetPos();
-        for (int i = 0; i < otherPlayerInfos.Length; i++)
-        {
-            Vector3 otherPlayerPosition = new Vector3(otherPlayerInfos[i].X, otherPlayerInfos[i].Y, 0f);
+    private GameObject AddOtherPlayer(Info playerInfo)
+    {
+        Debug.Log("[PositionSynchronizer] 다른 플레이어 추가");
 
-            if (i >= otherPlayers.Count)
-            {
-                Debug.Log("추가");
-                otherPlayers.Add(Instantiate(
+        Vector3 otherPlayerPosition = new Vector3(playerInfo.X, playerInfo.Y, 0f);
+
+        otherPlayersGameObject.Add(playerInfo.Id, Instantiate(
                 otherPlayerPrefab,
                 otherPlayerPosition,
                 Quaternion.identity));
 
-                return;
-            }
+        return otherPlayersGameObject[playerInfo.Id];
+    }
 
-            otherPlayers[i].transform.position = otherPlayerPosition;
+    private void SyncThisClientPosition()
+    {
+        Server server = NetworkManager.Instance.Server;
+
+        Vector2 playerPosition = player.transform.position;
+        server.SetPos(new Info(playerPosition.x, playerPosition.y));
+    }
+
+    private void SyncOtherClientsPosition()
+    {
+        Server server = NetworkManager.Instance.Server;
+
+        Info[] otherPlayers = server.GetPos();
+
+        foreach (Info otherPlayer in otherPlayers)
+        {
+            activedOtherPlayersId.Add(otherPlayer.Id);
+
+            bool isAlreadyJoinedPlayer = otherPlayersGameObject.TryGetValue(otherPlayer.Id, out GameObject otherPlayerGameObject);
+            if (!isAlreadyJoinedPlayer)
+                otherPlayerGameObject = AddOtherPlayer(otherPlayer);
+
+            Vector2 otherPlayerPositon = new Vector2(otherPlayer.X, otherPlayer.Y);
+            otherPlayerGameObject.transform.position = otherPlayerPositon;
         }
+
+        foreach (KeyValuePair<int, GameObject> otherPlayer in otherPlayersGameObject)
+        {
+            int otherPlayerKey = otherPlayer.Key;
+
+            bool isDisconnectedPlayer = !activedOtherPlayersId.Contains(otherPlayerKey);
+            if (isDisconnectedPlayer)
+            {
+                Destroy(otherPlayer.Value);
+                otherPlayersGameObject.Remove(otherPlayer.Key);
+            }
+        }
+
+        activedOtherPlayersId.Clear();
     }
 }
